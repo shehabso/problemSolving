4dc1e791e82c17b15240522f04843d8402296b51
shehab93140 <shehab162519@gmail.com>
2024-02-01 19:09:00 +0200

    .

---
+++ Level_2/02-non-linear-data-structures/Hash Table/Jewels_and_Stones.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int c=0;
+        for(int i=0;i<stones.size();i++){
+            for(int j=0;j<jewels.size();j++){
+                if(jewels[j]==stones[i]) c++;
+            }
+        }
+        return c;
+    }
+
+};
