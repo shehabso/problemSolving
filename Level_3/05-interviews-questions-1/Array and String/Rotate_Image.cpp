d1d50b98695cd14b986efa8ce4b92861f694f323
shehab93140 <shehab162519@gmail.com>
2024-02-08 20:00:59 +0200

    .

---
+++ Level_3/05-interviews-questions-1/Array and String/Rotate_Image.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void transpose(vector<vector<int>>& matrix){
+        for (int i=0;i<matrix.size();i++){
+            for(int j=i;j<matrix[0].size();j++){
+                int temp = matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+    }
+    void revereRows(vector<vector<int>>& matrix){
+        for (int i=0;i<matrix.size();i++){
+            int left =0;
+            int right =matrix.size()-1;
+            while(left<right){
+                int temp =matrix[i][left];
+                matrix[i][left]=matrix[i][right];
+                matrix[i][right]=temp;
+                left++;
+                right--;
+            }
+        }
+    }
+    void rotate(vector<vector<int>>& matrix) {
+       transpose(matrix);
+       revereRows(matrix);
+    }
+};
