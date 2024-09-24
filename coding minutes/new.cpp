5c420b696303b53949305f8d1a1340df020b82be
shehab tarek <shehab162519@gmail.com>
2024-09-24 23:44:21 +0300

    array

--- coding minutes/new.cpp
+++ coding minutes/new.cpp
@@ -1,17 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main(){
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-
-    int n;
-    cin>>n;
-    cout<<n;
-    
+void updateArray(int arr[],int i,int val){
+    arr[i]=val;
 }
-
-
-
-
+void print (int arr[],int n){
+    for (int i = 0; i < n; i++)
+{
+    cout<<arr[i]<<" ";
+}
+}
+int main(){
+int arr [5]={1,2,3,4,5};
+int n=sizeof(arr)/sizeof(arr[0]);
+updateArray(arr,1,13);
+print(arr,n);
+    return 0;
+}
 	
