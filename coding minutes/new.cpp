6eceaebeafcf35d2c409c75f39c80a13783c7fd4
shehab tarek <shehab162519@gmail.com>
2024-09-24 23:32:33 +0300

    first commit

---
+++ coding minutes/new.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+
+    int n;
+    cin>>n;
+    cout<<n;
+    
+}
+
+
+
+
+	
