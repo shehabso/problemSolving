2facec78aab4a87a5b1dfedbfdb570a333adb25e
shehab tarek <58656735+shehabso@users.noreply.github.com>
2024-10-21 09:57:50 +0300

    Time: 0 ms (100%), Space: 15.1 MB (80.99%) - LeetHub

---
+++ 2058-concatenation-of-array/2058-concatenation-of-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> result(2*nums.size()) ;
+        for (int i=0;i<nums.size();i++){
+            result[i] =nums[i];
+        }
+        for (int i=0;i<nums.size();i++){
+            result[i+nums.size()] =nums[i];
+        }
+
+
+        return result ;
+        
+    }
+};
