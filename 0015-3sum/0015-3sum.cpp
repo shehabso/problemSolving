0ff836c815939080a9f1dddc5037091bfe603aca
shehab tarek <58656735+shehabso@users.noreply.github.com>
2024-11-02 15:31:32 +0200

    Time: 51 ms (63.23%), Space: 27.3 MB (97.12%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // the first thing make a 3 pionter
-        // -1   0  1  2  -1  -4
-        //   i   j             k
 
-        sort(nums.begin(),nums.end());
+        //   -4 -1 -1 0 1 2  
+        //    i  j        k  sum<0
+        //    i       j   k  sum<0
+        //    i         j k  sum<0
+        //       i  j     k  sum=0
+        //       i    j   k  sum>0
+        //       i    j k    sum=0
+
+
+
+        vector<vector<int>> result;
+        if (nums.size() < 3) {
+            return result;
+        }
+
+        sort(nums.begin(), nums.end());
         int n = nums.size();
-        int target=0;
-       
-        set<vector<int> > s;
-        vector <vector <int> > res;
-        for (int i = 0; i < n; i++) {
-            int j = i + 1;
-            int k = n - 1;
-            while (k>j){
-                int sum =nums[i]+nums[j]+nums[k];
-                if(sum==target){
-                    s.insert({nums[i],nums[j],nums[k]});
-                    j++;
-                    k--;
-                }
-                else if (sum < target){
-                    j++;
-                }
-                else {
-                    k--;
+        for (int i = 0; i < n - 1; i++) {
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    left++;
+                    while (nums[left] == nums[left - 1] && left < right)
+                        left++;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
                 }
             }
         }
-        for(auto triplets : s) {res.push_back(triplets);}
-        return res;
+        return result;
     }
 };
